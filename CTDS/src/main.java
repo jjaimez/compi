9b565edfb4a6d226e37af98a83d7a22d5f01adb4
jjaimez <jaimezjacinto@gmail.com>
2015-09-07 11:14:52 -0300

    primer entrega

--- CTDS/src/main.java
+++ CTDS/src/main.java
@@ -8,9 +8,19 @@ import java.io.FileReader;
 public class main {
 
     public static void main(String[] args) throws  Exception, RuntimeException {
-        for(int i =1; i< 24; i++){
+        for(int i =1; i< 22; i++){
             System.out.println("test"+i+ ".ctds" );
             BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/test/test"+i+".ctds"));
+            Lexer lex = new Lexer(br);
+            parser p = new parser(lex);
+            p.parse();
+            System.out.println("--------------------------");
+
+        }
+            System.out.println("<<<<<<<<<< Comienzan los test negativos >>>>>>>>>>");
+           for(int i =1; i< 9; i++){
+            System.out.println("test"+i+ ".ctds" );
+            BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/test/negativos/test"+i+".ctds"));
             Lexer lex = new Lexer(br);
             parser p = new parser(lex);
             p.parse();
